9bd8e38b5e73b494c3f224a5636b9412326b21a3
Victoria2343 <vikusya234@mail.ru>
2021-03-15 23:32:55 +0300

    init: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  for (let i=0; i < str.length; i++) {
+    let b = str[i];
+    if (b === '(' || b === '[' || b === '{' || b === '|') {
+      stack.push(b);
+    } else if (b === ')' && (stack[stack.length - 1] === '(')) {
+      stack.pop();
+    } else if (b === ']' && (stack[stack.length - 1] === '[')) {
+      stack.pop();
+    } else if (b === '}' && (stack[stack.length - 1] === '{')) {
+      stack.pop();
+    } else if (b === '|' && (stack[stack.length - 1] === '|')) {
+      stack.pop();
+    } 
+    else if ((b === ')' || b === ']' || b === '}' || b === '|') && (stack.length == 0)) {
+      return false;
+    }
+    else if ((stack.length == 0) && (b === '(' || b === '[' || b === '{' || b === '|') ) {
+      return false;
+    }  else {
+      return false;
+    }
+   
+  }
+  return true;
 }
